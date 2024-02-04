3dde0b13cca656c492b51722923f7bd167e65e43
Hai Phan <hai.phan@gmail.com>
2024-02-03 23:17:00 -0800

    first

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # service-broker-webclient
+Browser JavaScript library for communicating with the service broker
+
+## Install
+`npm install @service-broker/webclient`
+
+## Usage
+```javascript
+import { ServiceBroker } from "@service-broker/webclient"
+
+const sb = new ServiceBroker("wss://sb.mydomain.com", console)
+
+sb.request({name: "my-service"}, {payload: "my-request"})
+  .then(({payload}) => handleResult(payload))
+```
---
+++ dist/index.d.ts
@@ -0,0 +1,34 @@
+
+interface ServiceFilter {
+  name: string
+  capabilities?: string[]
+}
+
+export interface ServiceAdvert {
+  name: string
+  capabilities?: string[]
+  priority: number
+}
+
+export interface ServiceHandler {
+  (msg: Message): Partial<Message>|void|Promise<Partial<Message>|void>
+}
+
+interface Message {
+  header: any
+  payload: any
+}
+
+declare class ServiceBroker {
+  constructor(url: string, logger: {error: Console["error"], debug: Console["debug"]});
+  request<T>(service: ServiceFilter, req: unknown): Promise<T>;
+  requestTo<T>(endpointId: string, service: ServiceFilter, req: unknown): Promise<T>;
+  advertise(service: ServiceAdvert, handler: ServiceHandler): Promise<void>;
+  unadvertise(serviceName: string): Promise<void>;
+  setHandler(serviceName: string, handler: ServiceHandler): void;
+  publish(topic: string, text: string): Promise<void>;
+  subscribe(topic: string, handler: (text: string) => void): Promise<void>;
+  unsubscribe(topic: string): Promise<void>;
+  isConnected(): boolean;
+  addConnectListener(listener: () => void): void;
+}
